344985878dbe998dc265c1caab2e923248b21107
chloehsieh2001 <151177485+chloehsieh2001@users.noreply.github.com>
2023-11-17 23:49:38 +0800

    Add files via upload

---
+++ multicol.R
@@ -0,0 +1,23 @@
+d2015 <- with(df2015, data.frame(HappinessScore,GDP,Health,Freedom,Trust,Generosity))
+plot(d2015)
+
+d2016 <- with(df2016, data.frame(HappinessScore,GDP,Health,Freedom,Trust,Generosity))
+plot(d2016)
+
+d2017 <- with(df2017, data.frame(HappinessScore,GDP,Health,Freedom,Trust,Generosity))
+plot(d2017)
+
+d2018 <- with(clean.df2018, data.frame(HappinessScore,GDP,Health,Freedom,Trust,Generosity))
+plot(d2018)
+
+d2019 <- with(df2019, data.frame(HappinessScore,GDP,Health,Freedom,Trust,Generosity))
+plot(d2019)
+
+d2020 <- with(df2020, data.frame(HappinessScore,GDP,Health,Freedom,Trust,Generosity))
+plot(d2020)
+
+d2021 <- with(df2021, data.frame(HappinessScore,GDP,Health,Freedom,Trust,Generosity))
+plot(d2021)
+
+d2022 <- with(df2022, data.frame(HappinessScore,GDP,Health,Freedom,Trust,Generosity))
+plot(d2022)
